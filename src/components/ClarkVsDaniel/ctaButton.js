1504f91f395e1658be279dce98853a0f35a004bf
halaburdataras <taras.galaburda@outlook.com>
2026-03-13 21:26:49 +0200

    fix: cta button popup

--- src/components/ClarkVsDaniel/ctaButton.js
+++ src/components/ClarkVsDaniel/ctaButton.js
@@ -1,22 +1,11 @@
 "use client";
 
-import gsap from "gsap";
 import Button from "../Button";
 import s from "./ClarkVsDaniel.module.scss";
 
 const CtaButton = ({ children }) => {
-  const handleScroll = (e) => {
-    e.preventDefault();
-    gsap.to(window, { scrollTo: "#cta", ease: "power2" });
-  };
-
   return (
-    <Button
-      variant="primary"
-      href={"#cta"}
-      onClick={handleScroll}
-      className={s.ctaButton}
-    >
+    <Button variant="primary" href="#popup" className={s.ctaButton}>
       {children}
     </Button>
   );
