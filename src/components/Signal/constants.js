bf0694036ba6b2203f8a22bcdd5cce3c81d63b28
halaburdataras <taras.galaburda@outlook.com>
2026-03-23 16:03:28 +0200

    feat: develop Signal section

---
+++ src/components/Signal/constants.js
@@ -0,0 +1,114 @@
+export const BG_MESSAGES = [
+  {
+    id: "bg1",
+    icon: {
+      src: "/images/signal/slack.svg",
+      alt: "Slack",
+      width: 32,
+      height: 32,
+    },
+    title: "@here, I just got off a meeting with",
+    text: "need to migrate all of their users to the new instance for this to work for them...",
+  },
+  {
+    id: "bg2",
+    icon: {
+      src: "/images/signal/gcal.svg",
+      alt: "Google Calendar",
+      width: 32,
+      height: 32,
+    },
+    title: "Product Planning",
+    text: "4:00 PM - 5:00 PM, Google Meet",
+  },
+  {
+    id: "bg3",
+    icon: {
+      src: "/images/signal/gmail.svg",
+      alt: "Gmail",
+      width: 28,
+      height: 22,
+    },
+    title: "Onboarding timeline",
+    text: "Hi team, I just spoke with Acme and they’re hoping to complete onboarding before the end of the quarter...",
+  },
+  {
+    id: "bg4",
+    icon: {
+      src: "/images/signal/gmail.svg",
+      alt: "Gmail",
+      width: 28,
+      height: 22,
+    },
+    title: "Acme onboarding timeline",
+    text: "Hi team, I just spoke with Acme and they’re hoping to complete onboarding before the end of the quarter...",
+  },
+  {
+    id: "bg5",
+    icon: {
+      src: "/images/signal/jira.svg",
+      alt: "Jira",
+      width: 32,
+      height: 32,
+    },
+    title: "User Migration Bug",
+    text: "Melida at Acme Corp said that when she uses the migration page, it breaks...",
+  },
+  {
+    id: "bg6",
+    icon: {
+      src: "/images/signal/jira.svg",
+      alt: "Jira",
+      width: 32,
+      height: 32,
+    },
+    title: "Onboarding Bug",
+    text: "Melida at Acme Corp said that when she uses the migration page, it breaks...",
+  },
+];
+
+export const FG_MESSAGES = [
+  {
+    id: "jira",
+    icon: {
+      src: "/images/signal/jira.svg",
+      alt: "Jira",
+      width: 24,
+      height: 24,
+    },
+    title: "Jira Cloud",
+    type: "jira",
+  },
+  {
+    id: "slack",
+    icon: {
+      src: "/images/signal/slack.svg",
+      alt: "Slack",
+      width: 32,
+      height: 32,
+    },
+    type: "slack",
+  },
+  {
+    id: "zoom",
+    icon: {
+      src: "/images/signal/zoom.svg",
+      alt: "Zoom",
+      width: 36,
+      height: 36,
+    },
+    title: "Customer Onboarding",
+    type: "zoom",
+  },
+  {
+    id: "gmail",
+    icon: {
+      src: "/images/signal/gmail.svg",
+      alt: "Gmail",
+      width: 32,
+      height: 32,
+    },
+    title: "Melinda from Acme",
+    type: "gmail",
+  },
+];
